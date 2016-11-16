60d66200331ac4cac681c298f13e65444a483328
davidblancolopez <daviddbl94@gmail.com>
2016-11-16 19:20:20 +0100

    Cambios.

--- src/activitat/pkg1/pkg5/m9/uf2/Activitat15M9UF2.java
+++ src/activitat/pkg1/pkg5/m9/uf2/Activitat15M9UF2.java
@@ -1,13 +1,18 @@
 package activitat.pkg1.pkg5.m9.uf2;
 
+import java.util.concurrent.Semaphore;
+
 public class Activitat15M9UF2 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-
+        Semaphore semafor = new Semaphore(1, true);
+        
+        
     }
+    
 
     /*
     public void ingressar(float diners) {
@@ -32,5 +37,70 @@ public class Activitat15M9UF2 {
         sendSignal();
     }
     */
+//    
+//    
+//import java.util.concurrent.locks.*;
+//import java.util.concurrent.Semaphore;
+//
+//public class SemaphoreExample {
+//
+//int i = 0;
+//
+//public static void main (String[] args) {
+//
+//final SemaphoreExample example = new SemaphoreExample();
+//
+//final Semaphore semaphore = new Semaphore (1);
+//
+//final Runnable r = new Runnable () {
+//
+//public void run () {
+//
+//while (true) {
+//
+//try {
+//
+//semaphore.acquire();
+//
+////Sección crítica a proteger
+//
+//example.printSomething ();
+//
+//Thread.sleep (1000);
+//
+//semaphore.release();
+//
+//} catch (Exception ex) {
+//
+//System.out.println (" — Interrupted…");
+//
+//ex.printStackTrace ();
+//
+//}
+//
+//}
+//
+//}
+//
+//};
+//
+//new Thread (r).start ();
+//
+//new Thread (r).start ();
+//
+//new Thread (r).start ();
+//
+//}
+//
+//public void printSomething (){
+//
+//i++;
+//
+//System.out.println (" — current value of the i :"+ i);
+//
+//}
+//}  
+
+
     
 }

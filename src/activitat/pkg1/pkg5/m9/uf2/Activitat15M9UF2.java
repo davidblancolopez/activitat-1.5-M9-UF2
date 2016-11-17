74d9d78461c75d402f64467fd300181a29b21f43
davidblancolopez <daviddbl94@gmail.com>
2016-11-17 16:01:10 +0100

    Cambios.

--- src/activitat/pkg1/pkg5/m9/uf2/Activitat15M9UF2.java
+++ src/activitat/pkg1/pkg5/m9/uf2/Activitat15M9UF2.java
@@ -1,20 +1,62 @@
 package activitat.pkg1.pkg5.m9.uf2;
 
+import java.util.concurrent.locks.*;
 import java.util.concurrent.Semaphore;
 
 public class Activitat15M9UF2 {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        Semaphore semafor = new Semaphore(1, true);
-        
-        
-    }
-    
+    float saldo = 2000;
 
-    /*
+
+    public static void main(String[] args) {
+
+        final Activitat15M9UF2 example = new Activitat15M9UF2();
+
+        final Semaphore semaphore = new Semaphore(1);
+
+        final Runnable r = new Runnable() {
+
+            public void run() {
+
+                while (true) {
+
+                    try {
+
+                        semaphore.acquire();
+
+                        //Sección crítica a proteger
+                        example.printSomething();
+
+                        Thread.sleep(1000);
+
+                        semaphore.release();
+
+                    } catch (Exception ex) {
+
+                        System.out.println(" — Interrupted…");
+
+                        ex.printStackTrace();
+
+                    }
+
+                }
+
+            }
+
+        };
+
+        new Thread(r).start();
+
+        new Thread(r).start();
+
+    }
+
+    public void printSomething() {
+
+        System.out.println("Saldo actual: " + saldo );
+
+    }
+
     public void ingressar(float diners) {
         sendWait();
         float aux;
@@ -24,9 +66,7 @@ public class Activitat15M9UF2 {
         guardarSaldo(saldo);
         sendSignal();
     }
-    */
-    
-    /*
+
     public void treure(float diners) {
         sendWait();
         float aux;
@@ -36,71 +76,14 @@ public class Activitat15M9UF2 {
         guardarSaldo(saldo);
         sendSignal();
     }
-    */
-//    
-//    
-//import java.util.concurrent.locks.*;
-//import java.util.concurrent.Semaphore;
-//
-//public class SemaphoreExample {
-//
-//int i = 0;
-//
-//public static void main (String[] args) {
-//
-//final SemaphoreExample example = new SemaphoreExample();
-//
-//final Semaphore semaphore = new Semaphore (1);
-//
-//final Runnable r = new Runnable () {
-//
-//public void run () {
-//
-//while (true) {
-//
-//try {
-//
-//semaphore.acquire();
-//
-////Sección crítica a proteger
-//
-//example.printSomething ();
-//
-//Thread.sleep (1000);
-//
-//semaphore.release();
-//
-//} catch (Exception ex) {
-//
-//System.out.println (" — Interrupted…");
-//
-//ex.printStackTrace ();
-//
-//}
-//
-//}
-//
-//}
-//
-//};
-//
-//new Thread (r).start ();
-//
-//new Thread (r).start ();
-//
-//new Thread (r).start ();
-//
-//}
-//
-//public void printSomething (){
-//
-//i++;
-//
-//System.out.println (" — current value of the i :"+ i);
-//
-//}
-//}  
 
-
+    public float llegirSaldo(){  
+        return saldo;
+    }
+    
+    public void guardarSaldo(float saldoActualitzat){
+        saldo = saldoActualitzat;
+    }
+    
     
 }
---
+++ src/activitat/pkg1/pkg5/m9/uf2/ingresarDiners.java
@@ -0,0 +1,7 @@
+
+package activitat.pkg1.pkg5.m9.uf2;
+
+
+public class ingresarDiners {
+    
+}

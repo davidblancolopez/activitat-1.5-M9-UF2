c936d4b177b4e9656e6ac57fa89588ce6f28792b
davidblancolopez <daviddbl94@gmail.com>
2016-11-18 00:29:58 +0100

    Cambios.

--- src/activitat/pkg1/pkg5/m9/uf2/Activitat15M9UF2.java
+++ src/activitat/pkg1/pkg5/m9/uf2/Activitat15M9UF2.java
@@ -12,8 +12,15 @@ public class Activitat15M9UF2 {
         //Declaració del semafor
         final Semaphore semaphore = new Semaphore(1, true);
 
+        /**
+         * Creem el primer run que serà per fer els ingressos al compte.
+         */
         final Runnable runIngres = new Runnable() {
 
+            /**
+             * Aquest run executara el metode ingressar.
+             * Es bloqueja l'acces al semafor amb acquire i es permet amb release.
+             */
             public void run() {
                 try {
                     semaphore.acquire();
@@ -26,8 +33,15 @@ public class Activitat15M9UF2 {
             }
         };
 
+        /**
+         * Creem el segón run que serà per a fer les extraccions de diners del
+         * compte.
+         */
         final Runnable runTreure = new Runnable() {
-
+            /**
+             * Aquest run executara el metode treure.
+             * Es bloqueja l'acces al semafor amb acquire i es permet amb release.
+             */
             public void run() {
                 try {
                     semaphore.acquire();
@@ -41,6 +55,10 @@ public class Activitat15M9UF2 {
             }
         };
 
+        /**
+         * Aquest bucle serveix per a crear els 2 fils (Ingressar, treure),
+         * cadascún s'executara 5 vegades i es podra veure com actua el semafor.
+         */
         for (int i = 0; i < 5; i++) {
             new Thread(runIngres).start();
             new Thread(runTreure).start();
@@ -48,6 +66,14 @@ public class Activitat15M9UF2 {
 
     }
 
+    /**
+     * Aquest metode s'encarrega d'ingressar els diners al compte. Agafa el
+     * saldo actual i el introdueix a la variable aux, a aquesta variable se li
+     * sumen els diners que es volen ingressar y al final es guarda al saldo el
+     * valor de aux.
+     *
+     * @param diners
+     */
     public static void ingressar(float diners) {
         float aux, saldo;
         aux = llegirSaldo();
@@ -56,6 +82,14 @@ public class Activitat15M9UF2 {
         guardarSaldo(saldo);
     }
 
+    /**
+     * Aquest metode s'encarrega d'extreure els diners del compte. Agafa el
+     * saldo actual i el introdueix a la variable aux, a aquesta variable se li
+     * resten els diners que es volen extreure y al final es guarda al saldo el
+     * valor de aux.
+     *
+     * @param diners
+     */
     public static void treure(float diners) {
 
         float aux;
@@ -66,10 +100,18 @@ public class Activitat15M9UF2 {
 
     }
 
+    /**
+     *Aquest metode retorna el valor actual de saldo.
+     * @return
+     */
     public static float llegirSaldo() {
         return saldo;
     }
 
+    /**
+     * Aquest metode modifica el valor de saldo per el valor que se li passa per parametre.
+     * @param saldoActualitzat 
+     */
     public static void guardarSaldo(float saldoActualitzat) {
         saldo = saldoActualitzat;
     }
--- src/activitat/pkg1/pkg5/m9/uf2/semafor.java
+++
@@ -1,7 +0,0 @@
-
-package activitat.pkg1.pkg5.m9.uf2;
-
-
-public class semafor {
-    
-}
